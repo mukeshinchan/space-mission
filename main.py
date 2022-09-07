aab90b7c804f6328d0c4e50093e31c1560ad3d91
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-09-07 11:51:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from numerize import numerize
 df=pd.read_csv('space_missions.csv',encoding='ISO-8859-1')
-
-
 st.set_page_config(layout="wide")
-
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://wallpapers.com/images/high/4k-space-eye-formation-unmdcbrhea9nngay.jpg");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 plot=st.empty()
-
 with plot.container():
     col1,col2,col3=st.columns(3)
     with col1:
@@ -37,5 +22,3 @@ with plot.container():
         fig.update_layout(xaxis=dict(showgrid=False),yaxis=dict(showgrid=False))
         fig.update_layout({'plot_bgcolor':'rgba(0,0,0,0)','paper_bgcolor':'rgba(0,0,0,0)'})
         st.plotly_chart(fig)
-
-                
