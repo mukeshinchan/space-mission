1a423bf94aedc47f9a48d211d68bc411623e8c58
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-09-07 10:52:55 +0530

    d

--- main.py
+++ main.py
@@ -25,16 +25,7 @@ plot=st.empty()
 with plot.container():
     col1,col2,col3=st.columns(3)
     with col1:
-        df['Date']=pd.to_datetime(df['Date'])
-        df['Year']=df['Date'].dt.year
-        out = df.groupby(['Year']).count()
-        out.reset_index(inplace = True)
-        fig=px.line(out,x=out['Year'],y=out['Mission'])
-        fig.update_traces(line_color='#39bbf7')
-        fig.update_layout(xaxis=dict(showgrid=False),yaxis=dict(showgrid=False))
-        fig.update_layout({'plot_bgcolor':'rgba(0,0,0,0)','paper_bgcolor':'rgba(0,0,0,0)'})
-        st.plotly_chart(fig)
-
+       st.text('List of World Space Explorations Mission Space Mission is a journey, by a manned or unmanned vehicle, into space to gather scientific data. It is important for global partnerships and exploration capabilities that help global preparedness for protecting the Earth from catastrophic. In this article, we are giving the list of World Space Explorations Mission which is very useful for the competitive examinations like UPSC-prelims, SSC, State Services, NDA, CDS, and Railways etc.)
         
     with col2:
         df['Date']=pd.to_datetime(df['Date'])
